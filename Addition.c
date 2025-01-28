ab99b054df60ae3c254ebde87a959200398820fb
Hujjathullah-S-T <151118124+Hujjathullah-S-T@users.noreply.github.com>
2025-01-28 14:52:50 +0530

    Create Addition.c

---
+++ Addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+int a,b,c;
+printf("Enter First Number : ");
+scanf("%d",&a);
+printf("Enter Second Number : ");
+scanf("%d",&b);
+c=a+b;
+printf("The Sum is %d",c);
+}
